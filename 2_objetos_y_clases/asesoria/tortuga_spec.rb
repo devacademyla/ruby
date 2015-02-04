5b6d86df1277e96f206cc64a0a42010176700c6a
Kattya Cuevas <kattya.cuevas@gmail.com>
2015-02-04 16:09:58 -0500

    add spec class Dado

--- 2_objetos_y_clases/asesoria/tortuga_spec.rb
+++ 2_objetos_y_clases/asesoria/tortuga_spec.rb
@@ -2,38 +2,17 @@ require_relative './tortuga'
 
 RSpec.describe 'Tortuga' do
   describe 'Dado' do
-    it '#valor' do
-      dado = Dado.new
-      expect(dado.valor).to be_between(1, 6).inclusive
+    before do
+      @dado = Dado.new
     end
-  end
 
-  describe 'Cubilete' do
-    it '#dados' do
-      cubilete = Cubilete.new(4)
-      expect(cubilete.dados.length).to eq 4
+    it '#new' do
+      expect(@dado.resultado).to eq 0
     end
-  end
 
-  describe 'Jugada' do
-    describe '#new' do
-      it 'sin caparazon ni patas' do
-        jugada = Jugada.new(0, 0)
-        expect(jugada.caparazon).to be >= 0
-        expect(jugada.patas).to be >= 0
-      end
-
-      it 'con caparazon sin patas' do
-        jugada = Jugada.new(1, 0)
-        expect(jugada.caparazon).to eq 1
-        expect(jugada.patas).to be >= 0
-      end
-
-      it 'con caparazon y una pata' do
-        jugada = Jugada.new(1, 1)
-        expect(jugada.caparazon).to eq 1
-        expect(jugada.patas).to be >= 1
-      end
+    it '#lanzar' do
+      @dado.lanzar
+      expect(@dado.resultado).to be_between(1, 6).inclusive
     end
   end
 end
