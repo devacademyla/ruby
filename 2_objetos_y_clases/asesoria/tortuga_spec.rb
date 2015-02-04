302279925df87eff5548d79528e3aa0a96592c44
Kattya Cuevas <kattya.cuevas@gmail.com>
2015-02-04 18:20:43 -0500

    editar specs tortuga

--- 2_objetos_y_clases/asesoria/tortuga_spec.rb
+++ 2_objetos_y_clases/asesoria/tortuga_spec.rb
@@ -6,28 +6,47 @@ RSpec.describe 'Tortuga' do
       @dado = Dado.new
     end
 
-    it '#new' do
-      expect(@dado.resultado).to eq 0
-    end
-
-    it '#lanzar' do
-      @dado.lanzar
-      expect(@dado.resultado).to be_between(1, 6).inclusive
+    describe '#lanzar' do
+      it 'el resultado es un número aleatorio entre 1 y 6' do
+        @dado.lanzar
+        expect(@dado.resultado).to be_between(1, 6).inclusive
+      end
     end
   end
 
   describe 'Cubilete' do
     before do
       @cubilete = Cubilete.new
+      @cubilete.cargar(5)
+    end
+
+    describe '#cargar' do
+      it 'el cubilete tiene 5 dados' do
+        expect(@cubilete.resultados.length).to eq 5
+      end
+    end
+
+    describe '#lanzar' do
+      it 'devolver un arreglo de numeros aleatorios' do
+        @cubilete.lanzar
+        @cubilete.resultados.each do |resultado|
+          expect(resultado).to be_between(1, 6).inclusive
+        end
+      end
+    end
+  end
+
+  describe 'Tortuga' do
+    before do
+      @tortuga = Tortuga.new
     end
 
     it '#new' do
-      expect(@cubilete.resultados.length).to eq 0
+      resultado = 'Obtuviste 0 caparazon y 0 patas en 0/3 jugadas'
+      expect(@tortuga.resultado).to eq resultado
     end
 
-    it '#cargar' do
-      @cubilete.cargar(5)
-      expect(@cubilete.resultados.length).to eq 5
+    it '#evaluar' do
     end
   end
 end
